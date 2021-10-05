5288844f1f18d48eb55619b49c244513e8118548
Utsav-Mehta <Utsav.mce20@sot.pdpu.ac.in>
2021-10-05 22:42:03 +0530

    Adding Dark Mode feature for the App. Some basic changes in appearance of buttons.

---
+++ app/src/main/java/com/example/quizapp/Settings.kt
@@ -0,0 +1,32 @@
+package com.example.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.quizapp.R
+
+class Settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        val darkmode=findViewById(R.id.darkmode) as TextView
+        val back=findViewById(R.id.back) as ImageView
+        val changeName=findViewById(R.id.changeName) as TextView
+        darkmode.setOnClickListener{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            val intent = Intent(this, Quiz_questions::class.java)
+            startActivity(intent)
+        }
+        changeName .setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        back.setOnClickListener{
+            val intent = Intent(this, Quiz_questions::class.java)
+            startActivity(intent)
+        }
+    }
+}
