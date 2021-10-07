c3e0456a1a8e594adec8225e3a91c2d03e5b8bf1
mukulasija <lit2020010@iiitl.ac.in>
2021-10-07 13:39:04 +0530

    added option to return back to light mode

--- app/src/main/java/com/example/quizapp/Settings.kt
+++ app/src/main/java/com/example/quizapp/Settings.kt
@@ -1,6 +1,7 @@
 package com.example.quizapp
 
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -12,13 +13,35 @@ class Settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        val username = intent.getStringExtra("username").toString()
         val darkmode=findViewById(R.id.darkmode) as TextView
+        val currentNightMode =   resources.configuration.uiMode and  Configuration.UI_MODE_NIGHT_MASK
+        when (currentNightMode) {
+            Configuration.UI_MODE_NIGHT_NO -> {
+                darkmode.text = "Enable Dark Mode"
+            } // Night mode is not active, we're using the light theme
+            Configuration.UI_MODE_NIGHT_YES -> {
+                darkmode.text = "Disable Dark Mode"
+            } // Night mode is active, we're using dark theme
+
+        }
+        val username = intent.getStringExtra("username").toString()
+
         val back=findViewById(R.id.back) as ImageView
         val changeName=findViewById(R.id.changeName) as TextView
         darkmode.setOnClickListener{
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            val currentNightMode =   resources.configuration.uiMode and  Configuration.UI_MODE_NIGHT_MASK
+            when (currentNightMode) {
+                Configuration.UI_MODE_NIGHT_NO -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                } // Night mode is not active, we're using the light theme
+                Configuration.UI_MODE_NIGHT_YES -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                } // Night mode is active, we're using dark theme
+
+            }
             onBackPressed()
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+//            onBackPressed()
 //            val intent = Intent(this, Quiz_questions::class.java)
 //            intent.putExtra("name",username)
 //            startActivity(intent)
