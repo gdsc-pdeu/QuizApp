1883797fe24f51f7d289997840cfb46576f0950d
Utsav-Mehta <Utsav.mce20@sot.pdpu.ac.in>
2021-10-10 10:24:04 +0530

    Feature for switching between dark mode and light mode

--- app/src/main/java/com/example/quizapp/Settings.kt
+++ app/src/main/java/com/example/quizapp/Settings.kt
@@ -1,6 +1,7 @@
 package com.example.quizapp
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -12,13 +13,37 @@ class Settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        val darkmode=findViewById(R.id.darkmode) as TextView
+        val darkMode=findViewById(R.id.darkmode) as TextView
         val back=findViewById(R.id.back) as ImageView
         val changeName=findViewById(R.id.changeName) as TextView
-        darkmode.setOnClickListener{
+
+        val appSettingPrefs: SharedPreferences = getSharedPreferences("AppSettingPrefs", 0)
+        val sharedPrefsEdit: SharedPreferences.Editor = appSettingPrefs.edit()
+        val isNightModeOn: Boolean = appSettingPrefs.getBoolean("NightMode", false)
+
+        if(isNightModeOn){
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            val intent = Intent(this, Quiz_questions::class.java)
-            startActivity(intent)
+            darkMode.text = "Disable Dark Mode"
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                darkMode.text = "Enable Dark Mode"
+        }
+
+        darkMode.setOnClickListener{
+            if(isNightModeOn){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                sharedPrefsEdit.putBoolean("NightMode", false)
+                sharedPrefsEdit.apply()
+
+                darkMode.text = "Enable Dark Mode"
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                sharedPrefsEdit.putBoolean("NightMode", true)
+                sharedPrefsEdit.apply()
+
+
+                darkMode.text = "Disable Dark Mode"
+            }
         }
         changeName .setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
